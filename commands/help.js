c32632fd986583b0d636de867d9f97f4b81f7d0e
GeoArchonMorax <korisawarezero@gmail.com>
2025-01-23 15:03:04 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,32 +1,41 @@
-const axios = require('axios'); // Make sure axios is installed and imported
+const axios = require('axios');
+
+const commandCategories = {
+  "Admin": ['addadmin', 'adminlist', 'removeadmin', 'removeuser', 'ban', 'viplist'],
+  "Ai": ['ai', 'cid'],
+  "System": ['compile', 'restart', 'shadowgarden', 'uptime'],
+  "Utilities": ['help', 'notify', 'prefix', 'sendmessage',],
+  "User": ['unban', 'waifu', 'developer'],
+
+};
 
 module.exports = {
   name: 'help',
   category: 'Info',
   execute: async (api, event, args, commands, prefix, admins, appState, sendMessage) => {
     const { threadID } = event;
-    let menuMessage = "====『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧: 』====\n\n";
-    let i = 1;
 
-    const sortedCommands = [...commands.entries()].sort((a, b) => a[0].localeCompare(b[0]));
-
-    for (const [commandName, command] of sortedCommands) {
-      menuMessage += `  ╭─╮\n  | 『 ${i++}.』  ${commandName}\n  ╰─────────────ꔪ\n`;
+    let menuMessage = "━━━━━━━━━━━━━━\n𝙰𝚟𝚊𝚒𝚕𝚊𝚋𝚕𝚎 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜:\n";
+    for (const category in commandCategories) {
+      if (commandCategories[category].length > 0) { //Only add categories with commands
+          menuMessage += `╭─╼━━━━━━━━╾─╮\n│ ${category}│\n`;
+          for (const command of commandCategories[category]) {
+              menuMessage += `│ > ${command}\n`;
+          }
+          menuMessage += `╰─━━━━━━━━━╾─╯\n`;
+      }
     }
 
+    menuMessage += `Meesage the developer if there's error on our Cid Kagenou Bot.\n`;
+    menuMessage += `Cid Kagenou Bot\n`;
+    menuMessage += `━━━━━━━━━━━━━━\nDeveloper; Aljur Pogoy \nAdmin; Ana sophia\n━━━━━━━━━━━━━━`;
+
     try {
-      await sendMessage(api, { threadID, message: menuMessage }); // Send text first
-
-      const imageUrl = "https://imgur.com"; // replace mo lang if gusto, mo may attachment image
-
-      const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-      const imageBuffer = Buffer.from(response.data, 'binary');
-
-      await sendMessage(api, { threadID, attachment: imageBuffer, type: 'image' }); // Send image
-
+      await sendMessage(api, { threadID, message: menuMessage });
     } catch (error) {
-      console.error("Error sending help message or image:", error);
-      await sendMessage(api, { threadID, message: "Error sending help.  Check console logs." });
+      console.error("Error sending help message:", error);
+      await sendMessage(api, { threadID, message: "Error sending help. Check console logs." });
     }
   },
 };
+  
